786db58c91b2504370f2f38fb6d1a6836d6ef66e
JChris246 <chris.401@live.com>
2019-12-19 16:25:06 -0400

    Tried to start adding music functions

--- bot.js
+++ bot.js
@@ -3,8 +3,11 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 
+let servers = {};
+
+const ytdl = require("ytdl-core");
 const signal = '!';
-const version = "2019-12-19 0:57"
+const version = "2019-12-19 2:26"
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -18,12 +21,12 @@ let bot = new Discord.Client({
    autorun: true
 });
 
-bot.on('ready', function (evt) {
+bot.on('ready', function (message, evt) { 
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+	logger.info(bot.username + ' - (' + bot.id + ')');
+	//console.log(message);
 });
-
 
 function sendMsg(cId, msg) {
 	bot.sendMessage({
@@ -44,15 +47,15 @@ function displayHelp(cId) {
 	});
 }
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', function (user, user_id, channelID, realMsg, message) {
     // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with signal const (`!`)
-    if (message.substring(0, signal.length) === signal) {
+	// It will listen for messages that will start with signal const (`!`)
+	if (message.d.author.username === "SR Bot") return; //ignore messages from self
+    if (realMsg.substring(0, signal.length) === signal) {
 		//convert text to lowercase, to make command case insensitive
-        var args = message.substring(signal.length).toLowerCase().split('Welcome To The Server My Niggah!');
-        var cmd = args[0];
-
-        args = args.splice(signal.length);
+        var args = realMsg.substring(signal.length).toLowerCase().split(" ");
+		var cmd = args[0];
+		
         switch(cmd) {
             // !ping
             case 'ping':
@@ -89,10 +92,65 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				displayHelp(channelID);
 				break;
 			case "clear": //this command may need the bot to be an admin
-				message.channel.bulkDelete(!arg[1] ? 100 : arg[1] > 100 || arg < 2 ? 2 : arg[1]); //complex :)
+				console.log(bot.channels[channelID]);
+				//bot.messageDeleteBulk(!args[1] ? 100 : args[1] > 100 || args < 2 ? 2 : args[1]); //complex :)
+				break;
+
+			//separate music functions from other commands
+			case "play":
+			case "pause":
+			case "skip":
+			case "stop":
+				music(channelID, message, cmd, args, user);
 				break;
 			default:
 				sendMsg(channelID, "Not sure I understand what it is you want...");
 		}
      }
 });
+
+function play(connection, server) {
+	server.dispatcher = connection.playStream(ytdl(server.queue[0], { filter: (format) => format.container === 'm4a' }));
+	server.queue.shift();
+
+	server.dispatcher.on("end", function() {
+		if (server.queue[0])
+			play(connection, server.queue[0], server);
+		//else disconnect i guess
+	})
+}
+
+function music(c_id, msg, action, args, user) {
+	if (!args[1])
+		sendMsg(c_id, "Kinda need a lil more info than that...");
+	else if (args.length > 2) {
+		//check for if person trying to search for a song
+	} else {
+		if (!servers[msg.d.guild_id]) servers[msg.d.guild_id] = {
+			queue: []
+		}; //create a queue for this server if it does not exist*/
+
+		//console.log(msg);
+		//console.log(bot.users);
+
+		/*if (!user.voiceChannel) 
+			sendMsg(c_id, "You need to be in the voice channel to have me play music");
+		else {*/
+			let server = servers[0];
+
+			//Get bot to join voice channel
+			console.log(msg.member);
+			bot.channels['654695814228082718'].join().then(function(connection) {
+				if (action === "play") {
+					server.queue.push(args[1]); //add link to queue
+					play(connection, server);
+				} else if (action === "stop")
+					sendMsg(c_id, "Programmers in get to this part yet srry...");//stop();
+				else if (action == "pause")
+					sendMsg(c_id, "Programmers in get to this part yet srry...");//pause();
+				else 
+					sendMsg(c_id, "Programmers in get to this part yet srry...");//skip();
+			})
+		//}
+	}
+}
