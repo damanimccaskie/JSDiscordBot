ca8b3850834b2e3d010c8537d6ee21c70cd6cc61
JChris246 <chris.401@live.com>
2019-12-22 23:35:47 -0400

    Minor updates
    + Added command calc
    * Fixed fliptext

--- commands/calc.js
+++ commands/calc.js
@@ -7,13 +7,17 @@ module.exports = {
     execute (channel, args){
         main = require("../helperFunctions.js")
         args = main.removeFirstArg(args);
-        //if(!args[1]) return main.post(channel, "Please input a calculation.");
+        if(!args[0]) {
+            main.post(channel, "Please input a calculation.");
+            return;
+        }
 
         let resp;
-        try{
+        try {
+            //Warning: Function "eval" has been renamed to "evaluate" in v6.0.0, please use the new function instead.
             resp = math.eval(args.join(" "));
         } catch (e) {
-            //return main.post(channel, "Sorry, please input a valid calculation.");
+            return main.post(channel, "Sorry, please input a valid calculation.");
         }
         
         const embed = new Discord.RichEmbed()
--- commands/fliptext.js
+++ commands/fliptext.js
@@ -4,13 +4,13 @@ const flip = require("flip-text");
 module.exports = {
     name: "fliptext",
     description: "fliptext command",
-    execute:(bot, channelID, args) => {
+    execute:(channel, args) => {
       main = require("../helperFunctions.js") 
       args = main.removeFirstArg(args).reverse();
       var flipped = [];
   
       for(i = 0; i < args.length; i++)
         flipped.push(flip(args[i]));
-      main.post(bot,channelID,flipped.join(" "));
+      main.post(channel ,flipped.join(" "));
  }
 }
