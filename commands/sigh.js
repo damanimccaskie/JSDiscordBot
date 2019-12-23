d902b67d4903a60bbf43c79dc3f89d4973ef1fff
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2019-12-23 10:12:18 -0400

    minor commands

---
+++ commands/sigh.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "sigh",
+    description: "sigh command",
+    execute(channel) {
+        main = require("../helperFunctions.js"); 
+       
+        const ascii = `
+\`\`\`
+  _______   _________    _________   ,        ,
+ /              |       /            |        |
+|               |      |             |        |
+|               |      |             |        |
+ \\_____,        |      |  _______,   |________|
+        \\       |      |         |   |        |
+         |      |      |         |   |        |
+         |      |      |         |   |        |
+  ______/   ____|____   \\________|   |        |
+\u200b
+\`\`\`
+`;
+
+main.post(channel, ascii);
+    }
+}
---
+++ commands/space.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "space",
+    description: "space command",
+    execute(channel, args) {
+        main = require("../helperFunctions.js");
+        args = main.removeFirstArg(args);
+    if (args.length < 1) {
+        main.post (channel, 'You must provide text to space out!');
+    }
+
+    let amount = 2;
+
+    if (!isNaN(args[0])) {
+        amount = parseInt(args[0]);
+        (amount < 1) && (amount = 1);
+        (amount > 15) && (amount = 15);
+        args = args.slice(1);
+    }
+
+    main.post(channel, args.join(' '.repeat(amount / 2)).split('').join(' '.repeat(amount)));
+}
+}
