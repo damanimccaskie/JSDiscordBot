fd50b7e71800558f50fac0201195a34c4bace3d3
JChris246 <chris.401@live.com>
2021-05-13 19:16:56 -0400

    feat: updated price command to use CoinGecko api instead of parsing coinmarketcap

--- commands/price.js
+++ commands/price.js
@@ -1,64 +1,39 @@
 module.exports = {
     name: "price",
     description: "command the price of a given cryptocurrency",
-    execute(channel, args) {
+    async execute(channel, args) {
         const main = require("../helperFunctions.js");
         const request = require("request");
 
         //if user passed args to search, join them together for the url
         search = "";
         for (i = 1; i < args.length; i++)
-            search += args[i] + "+";
+            search += args[i] + " ";
         search = search.trim().substring(0, search.length - 1);
 
-        let options = {
-            url: "https://coinmarketcap.com/",
-            method: "GET",
-            headers: {
-                "Accept": "text/html",
-                "User-Agent": "Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/71.0"
-            }
-        };
+        const CoinGecko = require('coingecko-api');
+        const CoinGeckoClient = new CoinGecko();
 
-        request(options, function (error, response, body) {
-            if (error) {
-                console.log(error);
-                return;
-            }
-
-            //parse page
-            const DomParser = require('dom-parser');
-            let parser = new DomParser();
-
-            let page = parser.parseFromString(body);
-
-            let coin = null;
-
-            Array.from(page.getElementsByTagName("tr")).forEach(tr => {
-                let nameCell = tr.childNodes[2].innerHTML;
-                let name = parser.parseFromString(nameCell).getElementsByTagName("p")[0];
-                if (!name)
-                    name  = parser.parseFromString(nameCell).getElementsByTagName("span")[1];
-                let price = tr.childNodes[3].textContent;
-                let change = tr.childNodes[4].textContent;
-
-                if (name.textContent.toLowerCase() == search.toLowerCase()) {
-                    coin = {
-                        name: name.textContent,
-                        price: price.replace("<!-- -->",""),
-                        change: change.replace("<!-- -->","")
-                    }
-                }
-            })
-
-            // post result to chat
-            if (coin != null) {
-                let response = "Coin: " + coin.name;
-                response += "\nPrice: " + coin.price;
-                if (coin.change.length > 0)
-                    response += "\n24hr Change: " + coin.change;
-                main.post(channel, response);
-            } else main.post(channel, "Couldnt find coin: "+search);
+        let data = await CoinGeckoClient.simple.price({
+            ids: [search],
+            vs_currencies: ['usd'],
+            include_24hr_vol: true,
+            include_24hr_change: true
         });
+
+        // post result to chat
+        try {
+            if (Object.entries(data.data).length != 0) {
+                let response = "Coin: " + search;
+                response += "\nPrice (usd): $" + data.data[search].usd;
+                if (data.data[search].usd_24h_vol) 
+                    response += "\n24hr Volume (usd): " + data.data[search].usd_24h_vol;
+                if (data.data[search].usd_24h_change) 
+                    response += "\n24hr Change (%): " + data.data[search].usd_24h_change;
+                main.post(channel, response)
+            } else main.post(channel, "Couldnt find coin: "+search);
+        } finally {
+            return data;
+        }
     }
 }
